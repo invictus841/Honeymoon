522525eba5b41c9a575291ea5c6598b9ca78777f
NT <usedforfunny@gmail.com>
2022-04-28 12:11:17 +0200

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Alexandre Talatinian on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
