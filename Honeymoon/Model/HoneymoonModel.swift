4e8dacfa2b6bcb9fb5aa238d949768ecb13854e4
NT <usedforfunny@gmail.com>
2022-04-28 23:29:57 +0200

    Added assets, LaunchScreen created, CardView created

---
+++ Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Alexandre Talatinian on 28/04/2022.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
